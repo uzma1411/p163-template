a1e7a1f3933b3bf0e3823de8d790d85fe8abe977
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-05-24 13:45:54 +0530

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("player-movement", {
+  init: function () {
+    this.walk();
+  },
+  walk: function () {
+    window.addEventListener("keydown", (e) => {
+
+      if (
+        //check e.key is equal to ArrowUp or  ArrowRight etc
+      ) {
+
+        // use query selector and refer to sound2 id
+
+        // call the playSound function for the entity
+        
+      }
+    });
+  },
+});
+
